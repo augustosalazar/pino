0981654d19e509646e046950dbc7f1910f273925
Augusto Salazar <augustosalazar@uninorte.edu.co>
2025-11-28 21:50:48 -0500

    fix double app bar on profile

--- r_pino/app/profile.tsx
+++ r_pino/app/profile.tsx
@@ -102,7 +102,7 @@ const styles = StyleSheet.create({
     },
     content: {
         padding: 20,
-        paddingTop: 60,
+        paddingTop: 20,
     },
     header: {
         flexDirection: 'row',
