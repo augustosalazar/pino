f77390df6140b9f13853f45ef166da525a17526d
Augusto Salazar <augustosalazar@uninorte.edu.co>
2025-12-15 10:38:55 -0500

    remove USE_GAMIFICATION_V2 flag

--- start_v2_server.bat
+++ start_v2_server.bat
@@ -1,5 +1,4 @@
 @echo off
 echo Iniciando PineServer con Gamification V2 Activado...
-set USE_GAMIFICATION_V2=true
 set PYTHONPATH=%PYTHONPATH%;%CD%
 python -m uvicorn pineServer.main:app --host 0.0.0.0 --port 8000 --reload
